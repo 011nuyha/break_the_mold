b5ab2bc9227a643efdeaac4665cddbe4012947f3
yewon <yewonlee11@gmail.com>
2023-04-11 16:09:46 +0900

    test

---
+++ plaga/hw_2_2.java
@@ -0,0 +1,23 @@
+package cp1_hw;
+
+public class hwhw2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10;
+		int y = 20;
+		int z = 30;
+		int a;
+		
+		a = x;
+		x = y;
+		y = z;
+		z = a;
+		
+		System.out.println("x : " +x);
+		System.out.println("y : " +y);
+		System.out.println("z : " +z);
+		
+	}
+
+}
